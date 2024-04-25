ecf7da4fe277e8b4c7358f91c85b1c5b1545744d
Marcio <m.bertelli236@gmail.com>
2024-04-25 20:51:33 -0300

    div com foreach

--- 99 - SALA/js/carregar.js
+++ 99 - SALA/js/carregar.js
@@ -4,15 +4,16 @@ tarefas.forEach( tarefa => card(tarefa) )
 
 function card(tarefa){
 
+    //template literals => ``
     const content = `
     <div class="nes-container with-title is-centered">
 
-        <p class="title">Figma</p>
-        <p>Criar um protótipo do site</p>
+        <p class="title">${tarefa.titulo}</p>
+        <p>${tarefa.descricao}</p>
 
         <a href="#" class="nes-badge is-icon">
             <span class="is-warning"><i class="nes-icon star is-small"></i></span> <!-- trocar a star por coin para alterar o icone-->
-            <span class="is-primary">150</span>
+            <span class="is-primary">${tarefa.pontos}</span>
         </a>
         <progress class="nes-progress is-success" value="50" max="100"></progress>
 
